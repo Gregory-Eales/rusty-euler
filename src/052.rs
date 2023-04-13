a58b5795b92dfcd0195ccd5a6a329fee9f0985eb
Gregory H Eales <gregory.hamilton.e@gmail.com>
2023-04-12 20:53:19 -0700

    try 052

---
+++ src/052.rs
@@ -0,0 +1,19 @@
+
+
+use::std::time::Instant;
+use::std::collections::HashMap;
+
+
+
+fn main() {
+
+
+    for i in 1..1_000_000_000 as u64 {
+
+    }
+}
+
+fn count_digits(n:u32) {
+    // counts the number of times 
+    // each digit 1-9 appears in 
+}
